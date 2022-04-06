7fc282350f1efa370900ac0379cbf7be005fca36
Alvaro Pavez <alvaro.pavezb@gmail.com>
2022-04-06 18:45:03 +1200

    feat: agregar preguntas

--- src/firebase/firebase.utils.jsx
+++ src/firebase/firebase.utils.jsx
@@ -1,5 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
@@ -12,20 +13,46 @@ const firebaseConfig = {
   measurementId: "G-CTMDMTC56C",
 };
 
-export const getTopics = async() => {
-    const markers = [];
-    await firebase.firestore().collection('topics').get()
-      .then(querySnapshot => {
-        querySnapshot.docs.forEach(doc => {
+export const getTopics = async () => {
+  const markers = [];
+  await firebase
+    .firestore()
+    .collection("topics")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.docs.forEach((doc) => {
         markers.push(doc.data());
       });
     });
-    return markers;
-  }
+  return markers;
+};
+
+export const addTopic = async (name, image, preguntas) => {
+  const storageRef = firebase.storage().ref("/" + image.name);
+  const task = storageRef.put(image);
+  task.on("state_changed", function complete(event) {
+    storageRef.getDownloadURL().then((url) => {
+      //img
+      const coverImage = url;
+      console.log(coverImage);
+      firebase.firestore().collection("topics").doc(name).set({
+        coverImage: coverImage,
+        preguntas: preguntas,
+      });
+      alert("Guardado");
+    });
+  });
+
+  /* await firebase.firestore().collection("topics").doc(name).set({
+    name: "test",
+  }); */
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export const firestore = firebase.firestore();
+
+const storage = firebase.storage();
 
 export default firebase;
